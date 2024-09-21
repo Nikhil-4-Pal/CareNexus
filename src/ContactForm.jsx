4984f97afda962039a7e8f4f57a1f6b183eca9c0
Nikhil-4-Pal <palnikhil9142@gmail.com>
2024-09-21 19:43:20 +0530

    Update ContactForm

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -21,13 +21,11 @@ const ContactForm = () => {
   };
 
   return (
-<<<<<<< HEAD
     <div className="contact-form h-full w-screen flex flex-col items-center justify-center ">
       <h2 className=' text-4xl mt-[2rem] text-blue-900 font-Raleway ' >Send your request</h2>
       <form  className=' bg-blue-300 shadow-md shadow-neutral-600 mt-[2rem] w-[33rem] h-[25rem] rounded-lg flex flex-col gap-[2%]  justify-center items-center ' onSubmit={handleSubmit}>
         <input
             className=' w-[30rem] h-[3rem] text-md font-sans rounded-md  font-bold focus:outline-none p-[1rem] '
-=======
     <div className="contact-form">
       <h2>Send your request</h2>
       <form onSubmit={handleSubmit}>
@@ -77,7 +75,4 @@ const ContactForm = () => {
 };
 
 
-export default ContactForm;
-=======
-export default ContactForm;
->>>>>>> origin/master
+export default ContactForm
