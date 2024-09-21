ddf5b79ec92dfcf309fb17ef86fb632fc5c048e6
palnikhil9813 <palnikhil9813@gmail.com>
2024-09-21 17:15:58 +0530

    commit resolved

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -25,11 +25,11 @@ const ContactForm = () => {
       <h2 className=' text-4xl mt-[2rem] text-blue-900 font-Raleway ' >Send your request</h2>
       <form  className=' bg-blue-300 shadow-md shadow-neutral-600 mt-[2rem] w-[33rem] h-[25rem] rounded-lg flex flex-col gap-[2%]  justify-center items-center ' onSubmit={handleSubmit}>
         <input
-            className=' w-[30rem] h-[3rem] text-md font-sans rounded-md  font-bold focus:outline-none p-[1rem] '
-    <div className="contact-form">
-      <h2>Send your request</h2>
-      <form onSubmit={handleSubmit}>
-        <input
+            className=' w-[30rem] h-[3rem] text-md font-sans rounded-md  font-bold focus:outline-none p-[1rem] '/>
+        <div>
+        <h2>Send your request</h2>
+        <form onSubmit={handleSubmit}>
+          <input
           type="text"
           name="name"
           placeholder="Name"
@@ -71,6 +71,9 @@ const ContactForm = () => {
         <button type="submit">Send</button>
       </form>
     </div>
+    </form>
+    </div>
+
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -37,28 +37,28 @@ import ContactPage from './ContactPage.jsx'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path='/' element={ <App/> } >
+      <Route path='/CareNexus' element={ <App/> } >
         <Route path='home' element={<Home/>} />
         <Route path='about' element={<About/>} />
         <Route path='contactus' element={<ContactPage/>} />
         <Route path='register' element={<ToggleBtwSignIn/>} />
         <Route path='Hospitals' element={<Hospital/>} />
-        <Route path='/Hospitals/AIIMS' element={<AIIMS/>} />
-        <Route path='/Hospitals/RML Hospital' element={<RML/>} />
-        <Route path='/Hospitals/Safdarjung Hospital' element={<AIIMS/>} />
-        <Route path='/Hospitals/Apollo Hospital' element={<Apollo/>} />
-        <Route path='/Hospitals/Fostis' element={<Deen_Dayal/>} />
-        <Route path='/profile' element={<Dashboard/>} />
-        <Route path='/oraganisationsignin' element={<SignInOrg/>} />
-        <Route path='/usersignin' element={<SignIn/>} />
-        <Route path='/services' element={<Services/>} />
-        <Route path='/services/EPS' element={<EPS/>} />
-        <Route path='/services/HBMS' element={<HBMS/>} />
-        <Route path='/services/HWMS' element={<HWMS/>} />
-        <Route path='/services/LMS' element={<LMS/>} />
-        <Route path='/services/PHM' element={<PHM/>} />
-        <Route path='/services/PMF' element={<PMF/>} />
-        <Route path='appointment' element={<PatientAppointmentForm/>} />
+        <Route path='Hospitals/AIIMS' element={<AIIMS/>} />
+        <Route path='Hospitals/RML Hospital' element={<RML/>} />
+        <Route path='Hospitals/Safdarjung Hospital' element={<AIIMS/>} />
+        <Route path='Hospitals/Apollo Hospital' element={<Apollo/>} />
+        <Route path='Hospitals/Fostis' element={<Deen_Dayal/>} />
+        <Route path='profile' element={<Dashboard/>} />
+        <Route path='oraganisationsignin' element={<SignInOrg/>} />
+        <Route path='usersignin' element={<SignIn/>} />
+        <Route path='services' element={<Services/>} />
+        <Route path='services/EPS' element={<EPS/>} />
+        <Route path='services/HBMS' element={<HBMS/>} />
+        <Route path='services/HWMS' element={<HWMS/>} />
+        <Route path='services/LMS' element={<LMS/>} />
+        <Route path='services/PHM' element={<PHM/>} />
+        <Route path='services/PMF' element={<PMF/>} />
+        <Route path='appointment' element={<Home/>} />
 
       </Route>
       
@@ -71,9 +71,5 @@ createRoot(document.getElementById('root')).render(
     <RouterProvider router={router} />
   </StrictMode>,
 )
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ContactPage />
-  </React.StrictMode>
-);
+
 
